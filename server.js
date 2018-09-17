0ec3453231cf5fa1bdac82baf1359ee2ee4c75c3
Sarah Phillips <snphillips@gmail.com>
2018-09-17 19:31:18 -0400

    database

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
 const http = require('http');
 const { Client } = require('pg');
 
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
 
 const PORT = process.env.PORT || 5000;
 const { DATABASE_URL } = process.env;
 
 
-const server = http.createServer((req, res) => {
-  const client = new Client({
-    connectionString: DATABASE_URL,
-  });
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  client.connect()
-    .then(() => client.query('SELECT * FROM citibike_rides LIMIT 100'))
-    .then((result) => {
-      res.end(`${result.rows[0].name}\n`);
-      client.end();
-    })
-    .catch(() => {
-      res.end('ERROR');
-      client.end();
-    });
+client.query('SELECT * FROM citibike_rides LIMIT 100;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
 });
+
+
 server.listen(PORT, () => {
   console.log(`Server running on ${PORT}. Let's ride!`);
 });
