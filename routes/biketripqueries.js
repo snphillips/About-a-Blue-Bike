601d4927f5c1d56815b4d659bcf67f658ec09920
Sarah Phillips <snphillips@gmail.com>
2018-09-17 20:01:50 -0400

    log of trips showing

--- routes/biketripqueries.js
+++ routes/biketripqueries.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const pool = require("../db");
+const pool = require("../server.js");
 const router = express.Router();
 
 
--- server.js
+++ server.js
@@ -1,27 +1,63 @@
 const http = require('http');
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
-const client = new Client({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  // ssl: true,
 });
 
-client.connect();
+pool.connect();
 
 
-const PORT = process.env.PORT || 5000;
+//  express is the library that makes this all possible
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+const cors = require('cors');
+
+//  Invoke express. Henseforth, app = express
+const app = express();
+
+app.use(bodyParser.json());
+// app.use(cors({ origin, credentials: true }));
+app.use('/', routes);
+
+
+const PORT = process.env.PORT || 4000;
 const { DATABASE_URL } = process.env;
 
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end("Hello World");
+});
 
-client.query('SELECT * FROM citibike_rides LIMIT 100;', (err, res) => {
+
+pool.query('SELECT * FROM citibike_rides LIMIT 5;', (err, res) => {
   if (err) throw err;
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
   }
-  client.end();
+  pool.end();
 });
+
+
+
+
+// router.get("/", (request, response, next) => {
+//   pool.query(
+//     "SELECT * FROM citibike_rides ORDER BY bikeid ASC LIMIT 10;",
+//     (err, res) => {
+//      console.log(err)
+//       if (err) return next(err);
+
+//       response.json(res.rows);
+//     }
+//   );
+// });
 
 
 server.listen(PORT, () => {
   console.log(`Server running on ${PORT}. Let's ride!`);
+  console.log(`Database URL is: ${process.env.DATABASE_URL}.`)
 });
